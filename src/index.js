80fecd6aea3a29f165a276ff6c8c44bedbbb930d
jancewicz <jancewicz.pawel1@gmail.com>
2024-03-12 00:10:36 +0100

    all test for html / css / pngs passed, webpack works

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import './style.css';
+import "./style.css";
+import Icon from './icons/ghlogo.png'
+
+
+function appendIcon() {
+    const newDiv = document.createElement("div");
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    newDiv.appendChild(myIcon);
+
+    return newDiv
+}
+
+document.body.appendChild(appendIcon())
 
 console.log("Webpack test")
 
